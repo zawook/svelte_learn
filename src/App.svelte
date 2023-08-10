2564e8edbbef6490a58af68a36d9e0e42a0386f8
zawook <zawookmist@gmail.com>
2023-08-11 04:41:18 +0900

    Nested

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,12 @@
 <script>
 	export let name;
+	import Nested from './Nested.svelte';
 </script>
 
 <main>
 	<h1>Hello {name}!</h1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<Nested />
 </main>
 
 <style>
---
+++ src/Nested.svelte
@@ -0,0 +1,1 @@
+<p>hello Nested.svelte</p>
