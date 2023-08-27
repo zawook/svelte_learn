695b1b220feec68d3f786e86c5f462baa8b82de2
zawook <zawookmist@gmail.com>
2023-08-28 04:20:42 +0900

    20230828_practice

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,35 @@
 <script>
 	export let name;
 	import Nested from './Nested.svelte';
-</script>
+	import Header from './header.svelte';
+	import Content from './content.svelte';
+	import Footer from './footer.svelte';
 
+	let count = 0; // state(상태값)
+
+	const handleClick = () => count = count + 1; // count를 count + 1로 재할당
+
+	let my = {
+		name: '박문우',
+		age: 37,
+		sex: 'male',
+	}
+</script>
+<Header />
 <main>
 	<h1>Hello {name}!</h1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
 	<Nested />
+	<Content />
+	<Content />
+	<button on:click={handleClick}>
+		클릭수 {count} <!-- { } 기호로 마크업에 상태값 표시-->
+	</button>
+	<h2>이름 : {my.name}</h2>
+	<h2>나이 : {my.age}</h2>
+	<h2>성별 : {my.sex}</h2>
 </main>
-
+<Footer />
 <style>
 	main {
 		text-align: center;
---
+++ src/content.svelte
@@ -0,0 +1,5 @@
+<script>
+    import SubContent from './subContent.svelte';
+</script>
+<h2>Content Space</h2>
+<SubContent />
