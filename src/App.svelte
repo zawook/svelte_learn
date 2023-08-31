5a9ab242e725efa7dd435647ca4c9efddb01594b
zawook <zawookmist@gmail.com>
2023-09-01 04:33:21 +0900

    20230901 $:

--- src/App.svelte
+++ src/App.svelte
@@ -14,6 +14,47 @@
 		age: 37,
 		sex: 'male',
 	}
+
+	let list = [
+		{
+			id:0,
+			content: 'todo 1st',
+			done: false,
+		},
+		{
+			id:1,
+			content: 'todo 2nd',
+			done: false,
+		},
+		{
+			id:2,
+			content: 'todo 3rd',
+			done: true,
+		},
+		{
+			id:3,
+			content: 'todo 4th',
+			done: false,
+		},
+	]
+	const handleAddTodo = () => {
+		const todo = {
+			id: list.length + 1,
+			content: 'new todo',
+			done: false
+		}
+		list = [...list, todo]
+	}
+
+	let count2 = 1;
+	function handleClick2() {
+		count2 += 1
+	}
+	$: doubled = count * 2
+	$: {
+		console.log( count )
+		console.log( doubled )
+	}
 </script>
 <Header />
 <main>
@@ -23,11 +64,26 @@
 	<Content />
 	<Content />
 	<button on:click={handleClick}>
-		클릭수 {count} <!-- { } 기호로 마크업에 상태값 표시-->
+		클릭수 {count} {count===1 ? 'time' : 'times'}<!-- { } 기호로 마크업에 상태값 표시-->
 	</button>
+	<p>{count}의 두 배 값 출력 : {doubled}</p>
 	<h2>이름 : {my.name}</h2>
 	<h2>나이 : {my.age}</h2>
 	<h2>성별 : {my.sex}</h2>
+	<h2>2023.09.01</h2>
+	<ul>
+		{#each list as item}
+		<li>
+			<p>{item.id}</p>
+			<p>{item.content}</p>
+			<p>{item.done}</p>
+		</li>
+		{/each}
+	</ul>
+	<button on:click={handleAddTodo}>할 일 추가</button>
+	<button on:click={handleClick2}>
+		클릭 수 {count2} {count2 === 1 ? 'time' : 'times'}
+	</button>
 </main>
 <Footer />
 <style>
